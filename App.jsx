cd215c7e1ac3afaab5e53e7cf09d22d3e31b744e
sha-hul <85384309+sha-hul@users.noreply.github.com>
2025-02-10 13:50:52 +0530

    1 bug fix on catch
    bug fix on catch

---
+++ App.jsx
@@ -0,0 +1,36 @@
+import CountryCard from "./Component/CountryCard";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const App = () => {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://xcountries-backend.azurewebsites.net/all"
+        );
+        setCountries(response.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+
+    return () => {
+      console.log("Clean Up");
+    };
+  }, []);
+
+  return (
+    <div id="wrapper">
+      {countries.map((item,index) => (
+        <CountryCard key={index} country={item} />
+      ))}
+    </div>
+  );
+};
+
+export default App;
